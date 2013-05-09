0592620c008abdc44cff2228dac75661f8d56bd1
Nolan Lawson <nolan.lawson@gmail.com>
2013-05-09 16:10:56 +0200

    wrote the codes to contact the server and manage hashchanges

--- js/application.js
+++ js/application.js
@@ -1,4 +1,44 @@
-$(function() {
+/*jshint bitwise:true, curly:true, eqeqeq:true, forin:true, noarg:true, noempty:true, nonew:true, undef:true, strict:true, browser:true */
+/*global console jQuery*/
+(function($) {
+    
+    "use strict";
+    
+    var serverUrl = 'http://localhost:8080/jnameconverter/convert';
+    
+    function convertName(params) {
+        if (params && params.q) {
+            $.ajax({
+                url      : serverUrl,
+                dataType : 'json',
+                data     : params
+                
+            }).
+            done(function(result){
+                window.alert(JSON.stringify(result));
+            }).
+            fail(function() {
+                console.log("failed to reach " + serverUrl);
+            });
+        }
+    }
+    
+    function setUpConvertButton() {
+        $('#btn-convert').click(function(){
+            
+            // ask server to convert
+            var q = $('#input-convert').val();
+            
+            if (q) {
+                
+                var hash = document.location.hash;
+                hash = hash.replace(/\?[^?]*$/,'');
+                hash = hash + '?' + $.param({q : q});
+                document.location.hash = hash;
+            }
+            
+        });
+    }
     
     function setUpLinks() {
         
@@ -6,7 +46,7 @@ $(function() {
         $('a[title]').each(function(){
             $(this).tooltip();
         }).click(function(e){
-            e.preventDefault()
+            e.preventDefault();
         });
         
         // internal links
@@ -17,7 +57,13 @@ $(function() {
     }
     
     function hashchange() {
-        var tabname = $.param.fragment() || 'home';
+        var tabname = ($.param.fragment() || 'home').replace(/\?[^?]*$/,'') || 'home';
+        
+        var params = $.param.fragment() && $.param.fragment().match(/\?([^?]*)$/);
+        if (params) {
+            params = $.deparam(params[1]);
+        }
+        
         
         var tabLinkSelector = 'a[href="#' + tabname + '"]';
         var tabContentSelector = '.tab-' + tabname;
@@ -27,10 +73,16 @@ $(function() {
             .parent().addClass('active');
         
         $('.tab-content').hide().filter(tabContentSelector).show();
+        
+        if (params) {
+            convertName(params);
+        }
+        
     }
     
     $(window).hashchange(hashchange).hashchange();
     
     setUpLinks();
+    setUpConvertButton();
     
-});
+})(jQuery);
