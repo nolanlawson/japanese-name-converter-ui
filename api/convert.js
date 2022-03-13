6e8ae2b9d86cc22643238160fc50e3e58ee3038a
Nolan Lawson <nolan@nolanlawson.com>
2022-03-13 11:52:22 -0700

    fix: use vercel serverless function

--- api/convert.js
+++ api/convert.js
@@ -1,14 +1,18 @@
+const { parse } = require('url')
+const fetch = require('node-fetch')
+
 const URL = 'https://jnameconverterapi.nolanlawson.com/JNameConverter'
 
 module.exports = async (req, res) => {
   try {
-    const q = req.params.q
-    const resp = await fetch(URL, { params: { q } })
-    const json = await resp.json()
-    res.setHeader('Cache-Control', 'maxage=0');
-    res.setHeader('Cache-Control', 's-maxage=604800');
-    res.setHeader('Content-Type', 'application/json; charset=utf-8');
-    res.send(JSON.stringify(json))
+    const { search } = parse(req.url)
+    const resp = await fetch(URL + search)
+    const text = await resp.text()
+
+    res.setHeader('Content-Type', 'application/json; charset=utf-8')
+    res.setHeader('Cache-Control', 'public,max-age=0,s-maxage=86400')
+    res.status(200)
+    res.send(text)
   } catch (error) {
     res.status(500);
     res.send(error.message);
